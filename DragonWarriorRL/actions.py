b3c6314f7708651cdbf0f7faf42d73b44e2646d7
pcountryman <antiprufrock@gmail.com>
2021-05-21 09:41:10 -0400

    Create base code for Dragon Warrior Reinforcement Learning bot. run_dragon_warrior.py calls agent created by dqn_agent.py, environment created by dragon_warrior_env.py and nes-py, and actions from actions.py.

---
+++ DragonWarriorRL/actions.py
@@ -0,0 +1,30 @@
+# setup actions for a very simple environment
+basic_actions = [
+    ['NOOP'],
+    ['right'],
+    ['left'],
+    ['up'],
+    ['down'],
+    ['A'],
+    ['B'],
+]
+
+no_b_actions = [
+    ['NOOP'],
+    ['right'],
+    ['left'],
+    ['up'],
+    ['down'],
+    ['A'],
+]
+
+dragon_warrior_actions = [
+    ['NOOP'],
+    ['right'],
+    ['left'],
+    ['up'],
+    ['down'],
+    ['A'],
+    ['B'],
+    ['start'],
+]
